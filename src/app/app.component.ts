6c0d967437b4b4f63b90018f29323c751321da32
SeungHyunKim85 <kshrtg85@gmail.com>
2019-06-18 12:52:18 +0900

    counter01 commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,29 +3,46 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <img width="300" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    <router-outlet></router-outlet>
+  <div class="container">
+    <button class="increase" (click)="increase($event)">+</button>
+    <div class="counter">{{ counter }}</div>
+    <button class="decrease" (click)="decrease($event)">-</button>
+  </div>
   `,
-  styles: []
+  styles: [`    
+  .container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 130px;
+    margin: 20px auto;
+    font-size: 24px;
+    color: #3f51b5;
+  }
+
+  button {
+    padding: 5px 10px;
+    font-size: 24px;
+    border-radius: 5px;
+    color: #3f51b5;
+    border-color: #3f51b5;
+    outline: none;
+    cursor: pointer;
+  }
+
+  .counter {
+    width: 50px;
+    text-align: center;
+  }`]
 })
 export class AppComponent {
   title = 'Counter01';
+  counter = 0;
+  increase() {
+    ++this.counter;
+  }
+  decrease() {
+    if(this.counter === 0) { return; }
+      this.counter--;
+  }
 }
